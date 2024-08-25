891d5f4cc6331bf5b7944fc42e74d3a9c8bec983
Michael Bohn <spaceodyssey@gmx.de>
2024-08-25 17:11:26 +0200

    column layout player UI, draft data layout catalog

--- src/bin/player/main.rs
+++ src/bin/player/main.rs
@@ -31,12 +31,22 @@ struct Track {
 }
 
 struct TrackList {
-    state: ListState,
-    items: Vec<Track>,
+    track_state: ListState,
+    tracks: Vec<Track>,
+}
+
+struct Game {
+    name: String,
+    track_list: TrackList,
+}
+
+struct GameList {
+    game_state: ListState,
+    games: Vec<Game>,
 }
 
 struct App {
-    tracks: TrackList,
+    game_list: GameList,
 }
 
 pub fn main() -> Result<(), String> {
@@ -73,17 +83,50 @@ pub fn main() -> Result<(), String> {
 impl App {
     fn new() -> App {
         App {
-            tracks: TrackList {
-                state: ListState::default(),
-                items: vec![
-                    Track {
+            game_list: GameList {
+                game_state: ListState::default(),
+                games: vec![
+                    Game {
                         name: "Wolfenstein 3D (1992)".to_string(),
+                        track_list: TrackList {
+                            track_state: ListState::default(),
+                            tracks: vec![
+                                Track {
+                                    name: "Track 01".to_string(),
+                                },
+                                Track {
+                                    name: "Track 02".to_string(),
+                                },
+                            ],
+                        },
                     },
-                    Track {
+                    Game {
                         name: "Duke Nukem (1996)".to_string(),
+                        track_list: TrackList {
+                            track_state: ListState::default(),
+                            tracks: vec![
+                                Track {
+                                    name: "Track 01".to_string(),
+                                },
+                                Track {
+                                    name: "Track 02".to_string(),
+                                },
+                            ],
+                        },
                     },
-                    Track {
+                    Game {
                         name: "Shadowcaster (1994)".to_string(),
+                        track_list: TrackList {
+                            track_state: ListState::default(),
+                            tracks: vec![
+                                Track {
+                                    name: "Track 01".to_string(),
+                                },
+                                Track {
+                                    name: "Track 02".to_string(),
+                                },
+                            ],
+                        },
                     },
                 ],
             },
@@ -98,8 +141,8 @@ impl App {
                 if key.kind == KeyEventKind::Press {
                     match key.code {
                         KeyCode::Char('q') => return Ok(()),
-                        KeyCode::Char('k') | KeyCode::Up => self.tracks.previous(),
-                        KeyCode::Char('j') | KeyCode::Down => self.tracks.next(),
+                        KeyCode::Char('k') | KeyCode::Up => self.game_list.previous(),
+                        KeyCode::Char('j') | KeyCode::Down => self.game_list.next(),
                         _ => {}
                     }
                 }
@@ -115,45 +158,55 @@ impl App {
 
 impl Widget for &mut App {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        let [playback_area, tracks_area] =
+        let [playback_area, bottom_area] =
             Layout::vertical([Constraint::Length(7), Constraint::Min(10)]).areas(area);
+
+        let [game_area, track_area] =
+            Layout::horizontal([Constraint::Percentage(50), Constraint::Percentage(50)])
+                .areas(bottom_area);
 
         Paragraph::new("TODO fill playback")
             .block(Block::bordered().title("Playback"))
             .render(playback_area, buf);
 
-        let list = List::new(
-            self.tracks
-                .items
+        let game_list = List::new(
+            self.game_list
+                .games
                 .iter()
                 .map(|track| track.name.clone())
                 .collect::<Vec<String>>(),
         )
         .highlight_style(Style::new().add_modifier(Modifier::REVERSED))
-        .block(Block::bordered().title("Tracks"));
-        StatefulWidget::render(list, tracks_area, buf, &mut self.tracks.state);
+        .block(Block::bordered().title("Games"));
+        StatefulWidget::render(game_list, game_area, buf, &mut self.game_list.game_state);
+
+        let track_list = List::new(Vec::<String>::new())
+            .highlight_style(Style::new().add_modifier(Modifier::REVERSED))
+            .block(Block::bordered().title("Tracks"));
+        Widget::render(track_list, track_area, buf);
+        //StatefulWidget::render(track_list, track_area, buf, &mut self.game_list.games[0].track_state);
     }
 }
 
-impl TrackList {
+impl GameList {
     fn previous(&mut self) {
-        let i = match self.state.selected() {
+        let i = match self.game_state.selected() {
             Some(i) => {
                 if i == 0 {
-                    self.items.len() - 1
+                    self.games.len() - 1
                 } else {
                     i - 1
                 }
             }
             None => 0,
         };
-        self.state.select(Some(i));
+        self.game_state.select(Some(i));
     }
 
     fn next(&mut self) {
-        let i = match self.state.selected() {
+        let i = match self.game_state.selected() {
             Some(i) => {
-                if i >= self.items.len() - 1 {
+                if i >= self.games.len() - 1 {
                     0
                 } else {
                     i + 1
@@ -161,7 +214,7 @@ impl TrackList {
             }
             None => 0,
         };
-        self.state.select(Some(i));
+        self.game_state.select(Some(i));
     }
 }
 
--- src/catalog/mod.rs
+++ src/catalog/mod.rs
@@ -1,1 +1,21 @@
 pub mod w3d;
+
+pub struct Track {
+    pub name: String,
+}
+
+pub struct Metadata {
+    pub name: String,
+    pub tracks: Vec<Track>,
+}
+
+pub enum Game {
+    W3D,
+    SOD,
+}
+
+// inferGame() -> Game ?
+// each catalog mod contains functions:
+// - is_game() -> bool
+// - load_track() -> Result<Vec<u8>, String>
+// - get_metadata() -> &'static Metadata
